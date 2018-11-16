ff7359ad4ab7f56e2f985bdf927218d3aeaaff0c
Josh Thomas <thomas.3186@osu.edu>
2018-11-15 21:51:16 -0500

    11/15/18 challenge completion

--- 11-15-18/index.js
+++ 11-15-18/index.js
@@ -1,23 +1,69 @@
 
 
+/**
+ * A Node which can have a value and up to two children.  Useful for building binary trees
+ * 
+ * @param {String} val the value for this root
+ * @param {Node} left (optional) the left child node 
+ * @param {Node} right (optional) the right child node 
+ */
 function Node (val, left, right) {
     this.val = val;
     this.left = left;
     this.right = right;
 }
 
-// 2(4()(3()()))()
+
+/**
+ * Serializes the binary tree to a string representation. The string will be in format:
+ *          'value(leftSubTree)(rightSubTree)'
+ * if a subtree is not present, '()' will be shown
+ * ex) dog(cat()(hamster()()))()
+ * 
+ * @param {Node} node the root node of a binary tree to serialize
+ * @returns {String} the string representation of the binary tree
+ */
 let serialize = function (node) {
     let rep = '';
 
-    if (node) {
-        rep = `${node.val}(${serialize(node.left)})(${serialize(node.right)})`
-    }
+    if (node) rep = `${node.val}(${serialize(node.left)})(${serialize(node.right)})`;
 
     return rep;
 }
 
+
+/**
+ * Deserializes a valid binary tree string representation to a binary tree object.
+ * 
+ * @param {String} rep the valid string representation of the binary tree
+ * @returns {Node} the root node of the binary tree
+ */
+let deserialize = function (rep) {
+
+    let createNode = function() {
+        let node;
+        if (rep.length > 0 && rep[0] !== ')') {
+            let valEndPos = rep.indexOf('(');
+            let val = rep.substring(0, valEndPos);
+
+            rep = rep.substring(valEndPos + 1);
+            let leftTree = createNode();
+            rep = rep.substring(2); // Remove ) and (
+            let rightTree = createNode();
+            rep = rep.substring(1); // Remove )
+            
+            node = new Node(val, leftTree, rightTree);
+
+        }
+        return node;
+    }
+
+    return createNode();
+}
+
+
 module.exports = {
     serialize,
+    deserialize,
     Node
 }
--- 11-15-18/test.js
+++ 11-15-18/test.js
@@ -1,22 +1,75 @@
 const expect = require('chai').expect;
-const { Node, serialize } = require('./index');
+const { Node, serialize, deserialize} = require('./index');
 
 describe('binary tree', function () {
 
     describe('serialize', function () {
         it('handles normal case', function () {
-            let root = new Node('root', new Node('left', new Node('left.left')), new Node('right'))
+            let root = new Node('root', new Node('left', new Node('left.left')), new Node('right'));
             let result = serialize(root);
             expect(result).to.be.a('string');
             expect(result).to.equal('root(left(left.left()())())(right()())');
         });
 
         
-        it('handles sing node', function () {
+        it('handles single node', function () {
             let root = new Node('root')
             let result = serialize(root);
             expect(result).to.be.a('string');
             expect(result).to.equal('root()()');
         });
+
+        it('handles empty', function () {
+            let root = undefined
+            let result = serialize(root);
+            expect(result).to.be.a('string');
+            expect(result).to.equal('');
+        });
     })
+
+
+    describe('deserialize', function () {
+        it('handles normal case', function () {
+            let strRep = 'root(left(left.left()())())(right()())';
+            let result = deserialize(strRep);
+            expect(result).to.be.a('object');
+            expect(result.val).to.equal('root');
+            expect(result.right.val).to.equal('right');
+        })
+
+        it('handles single root', function () {
+            let strRep = 'root()()';
+            let result = deserialize(strRep);
+            expect(result).to.be.a('object');
+            expect(result.val).to.equal('root');
+            expect(result.left).to.be.undefined;
+            expect(result.right).to.be.undefined;
+        });
+
+        it('handles empty string', function () {
+            let strRep = '';
+            let result = deserialize(strRep);
+            expect(result).to.be.undefined;
+        });
+    });
+
+    
+    describe('serialize and deserialize', function () {
+        it('handles normal case', function () {
+            let root = new Node('root', new Node('left', new Node('left.left')), new Node('right'));
+            let result = deserialize(serialize(root));
+            expect(result).to.be.a('object');
+            expect(result.val).to.equal('root');
+            expect(result.left.left.val).to.equal('left.left');
+        });
+
+        it('handles single node ', function () {
+            let root = new Node('root');
+            let result = deserialize(serialize(root));
+            expect(result).to.be.a('object');
+            expect(result.val).to.equal('root');
+            expect(result.left).to.be.undefined;
+        });
+    })
+    
 }) 
